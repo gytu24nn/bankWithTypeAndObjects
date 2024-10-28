ab7acbe009c15fa4a73469f28dab3b2550168303
Tuva Gyllensten <tuvagyllensten@gmail.com>
2024-10-28 18:36:36 +0100

    hämtat alla element från HTML om de behövs framåt

--- script.ts
+++ script.ts
@@ -6,6 +6,18 @@ const createAccountBtn = document.getElementById("createAccountBtn") as HTMLButt
 const loginPage = document.getElementById("loginPage") as HTMLDivElement;
 const createAccountPage = document.getElementById("createAccountPage") as HTMLDivElement;
 
+// element i logg in forumläret som kommer användas i klasserna senare
+const userNameInput = document.getElementById("userName") as HTMLInputElement;
+const userPasswordInput = document.getElementById("userPassword") as HTMLInputElement;
+const BtnLoggaIn = document.getElementById("BtnLoggaIn") as HTMLButtonElement;
+
+//Element till meny page där det olika alternativen som användaren kan välja mellan finns
+const menyPage = document.getElementById("menyPage") as HTMLDivElement;
+const balanceBtn = document.getElementById("balance") as HTMLHeadingElement;
+const depositBtn = document.getElementById("deposit") as HTMLHeadingElement;
+const withdrawBtn = document.getElementById("withdraw") as HTMLHeadingElement;
+const logOutBtn = document.getElementById("logOut") as HTMLHeadingElement;
+
 // Lägger till klick-händelse för att visa login-formuläret.
 loginBtn.addEventListener("click", () => {
     loginPage.classList.remove("hide");
